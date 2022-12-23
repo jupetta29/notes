9f282150a82a7d83614358ed7a36cabcb308a471
jupetta29 <121144716+jupetta29@users.noreply.github.com>
2022-12-22 23:19:40 -0600

    Add pizza_order_practice.py

---
+++ pizza_order_practice.py
@@ -0,0 +1,27 @@
+print('Welcome to Python Pizza Deliveries!')
+
+size = input('What size pizza do you want? S, M, or L ')
+add_pepperoni = input('Do you want pepperoni? Y or N ')
+extra_cheese = input('Do you want extra cheese? Y or N ')
+
+bill = 0
+
+if size.upper() == 'S':
+    bill += 15
+elif size.upper() == 'M':
+    bill += 20
+else:
+    bill += 25
+
+if add_pepperoni.upper() == 'Y':
+    if size.upper() == 'S':
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese.upper() == 'Y':
+    bill += 1
+
+message = f'Your final bill is: ${bill}.'
+
+print(message)
