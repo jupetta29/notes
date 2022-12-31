6fb308f557ff750c23fe9e2dc801fb85c8b7c73f
jupetta29 <121144716+jupetta29@users.noreply.github.com>
2022-12-30 20:59:06 -0600

    Add blackjack_art.py

---
+++ blackjack_art.py
@@ -0,0 +1,22 @@
+logo = """
+.------.            _     _            _    _            _    
+|A_  _ |.          | |   | |          | |  (_)          | |   
+|( \/ ).-----.     | |__ | | __ _  ___| | ___  __ _  ___| | __
+| \  /|K /\  |     | '_ \| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
+|  \/ | /  \ |     | |_) | | (_| | (__|   <| | (_| | (__|   < 
+`-----| \  / |     |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\\
+      |  \/ K|                            _/ |                
+      `------'                           |__/           
+"""
+
+# deal_more_cards = True
+
+#     while deal_more_cards:
+#         deal_another_card = input('Type "y" to get another card, type "n" to pass: ').lower()
+        
+#         if deal_another_card == 'y':
+#             user_next_card = random.choice(cards)
+#             user_cards.append(user_next_card)
+#             print(f'Your cards: {user_cards}, current score: {sum(user_cards)}')
+#         else:
+#             deal_more_cards = False
