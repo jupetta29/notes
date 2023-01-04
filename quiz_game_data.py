ed57756b393422c53698e4322fa15b9777063e56
jupetta29 <121144716+jupetta29@users.noreply.github.com>
2023-01-04 04:27:34 -0600

    Add quiz_game_data.py

---
+++ quiz_game_data.py
@@ -0,0 +1,25 @@
+question_data = [
+    {"question": "Linus Torvalds created Linux and Git.",
+     "correct_answer": "True"},
+    {"question": "The programming language 'Python'; is based off a modified "
+                 "version of 'JavaScript';.",
+     "correct_answer": "False"},
+    {"question": "The logo for Snapchat is a Bell.",
+     "correct_answer": "False"},
+    {"question": "Pointers were not used in the original C programming "
+                 "language; they were added later on in C++.",
+     "correct_answer": "False"},
+    {"question": "Ada Lovelace is often considered the first computer "
+                 "programmer.",
+     "correct_answer": "True"},
+    {"question": "'HTML'; stands for Hypertext Markup Language.",
+     "correct_answer": "True"},
+    {"question": "Time on Computers is measured via the EPOX System.",
+     "correct_answer": "False"},
+    {"question": "The Windows 7 operating system has six main editions.",
+     "correct_answer": "True"},
+    {"question": "The Windows ME operating system was released in the "
+                 "year 2000.",
+     "correct_answer": "True"},
+    {"question": "Linux was first created as an alternative to Windows XP.",
+     "correct_answer": "False"}]
